f658f5952ed8326bd77c444d439f83a1b0b31da0
LuisDaniel <luisdaniel9986@gmail.com>
2024-04-21 01:25:58 -0500

    fuera_fecha_remove

--- resources/views/consultas/ventas/alertas/envio.blade.php
+++ resources/views/consultas/ventas/alertas/envio.blade.php
@@ -266,17 +266,23 @@ function loadTable()
                     //         <i class='fa fa-times'></i> ANULAR
                     //     </button>`;
 
-                    if(data.code != '1033' && dias > 0)
-                    {
-                        cadena = cadena + `
-                        <button type='button' class='btn btn-sm btn-success m-1' onclick='enviarSunat(${data.id})'
-                            title='Enviar Sunat'>
-                            <i class='fa fa-send'></i> Sunat
-                        </button>`;
-                    }
-                    else {
-                        cadena = cadena + "<span class='badge badge-warning'>FUERA DE FECHA</span>";
-                    }
+                    // if(data.code != '1033' && dias > 0)
+                    // {
+                    //     cadena = cadena + `
+                    //     <button type='button' class='btn btn-sm btn-success m-1' onclick='enviarSunat(${data.id})'
+                    //         title='Enviar Sunat'>
+                    //         <i class='fa fa-send'></i> Sunat
+                    //     </button>`;
+                    // }
+                    // else {
+                    //     cadena = cadena + "<span class='badge badge-warning'>FUERA DE FECHA</span>";
+                    // }
+
+                    cadena = cadena + `
+                         <button type='button' class='btn btn-sm btn-success m-1' onclick='enviarSunat(${data.id})'
+                             title='Enviar Sunat'>
+                             <i class='fa fa-send'></i> Sunat
+                         </button>`;
 
                     return cadena;
                 }
