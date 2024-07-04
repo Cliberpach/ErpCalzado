e0685ee23a725e6a268a42cbd793ad18ffb36ca6
LuisDaniel <luisdaniel9986@gmail.com>
2024-07-04 10:35:11 -0500

    error venta

--- app/Listeners/ConsultarTipoNumeracion.php
+++ app/Listeners/ConsultarTipoNumeracion.php
@@ -43,12 +43,14 @@ class ConsultarTipoNumeracion
             //->join('cotizacion_documento','cotizacion_documento.empresa_id','=','empresas.id')
             ->where('empresa_numeracion_facturaciones.tipo_comprobante',$documento->tipo_venta)
             ->where('empresa_numeracion_facturaciones.empresa_id',$documento->empresa_id)
+            ->where('empresa_numeracion_facturaciones.estado',"ACTIVO")
             //->where('cotizacion_documento.tipo_venta',$documento->tipo_venta)
             //->where('cotizacion_documento.estado','!=','ANULADO')
             //->select('cotizacion_documento.*','empresa_numeracion_facturaciones.*')
             ->select('empresa_numeracion_facturaciones.*')
             //->orderBy('cotizacion_documento.correlativo','DESC')
             ->get();
+
             dd($serie_comprobantes);
 
             if (count($serie_comprobantes) == 1) {
