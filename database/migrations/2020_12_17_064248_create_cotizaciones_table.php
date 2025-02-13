7f29c08aa1510bc649816d4327948dc16dcd64e7
LuisDaniel <luisdaniel9986@gmail.com>
2025-02-12 19:50:08 -0500

    fix

--- database/migrations/2020_12_17_064248_create_cotizaciones_table.php
+++ database/migrations/2020_12_17_064248_create_cotizaciones_table.php
@@ -29,7 +29,7 @@ class CreateCotizacionesTable extends Migration
             $table->foreign('cliente_id')->references('id')->on('clientes');
             
             $table->unsignedBigInteger('condicion_id');
-            $table->foreign('sedecondicion_id_id')->references('id')->on('condicions');
+            $table->foreign('condicion_id')->references('id')->on('condicions');
 
             $table->unsignedBigInteger('registrador_id');
             $table->foreign('registrador_id')->references('id')->on('users');
