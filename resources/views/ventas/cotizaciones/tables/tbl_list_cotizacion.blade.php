e75394bed3d309b3e16b46810ba5b4ed122f520e
Daniel Alva <luisdaniel9986@gmail.com>
2025-06-09 10:05:04 -0500

    fix

--- resources/views/ventas/cotizaciones/tables/tbl_list_cotizacion.blade.php
+++ resources/views/ventas/cotizaciones/tables/tbl_list_cotizacion.blade.php
@@ -1,13 +1,13 @@
-<table class="table table-striped table-bordered table-hover" id="tbl_list_cotizaciones" style="text-transform:uppercase;width:100%;">
+<table class="table table-striped table-bordered table-hover" id="tbl_list_cotizaciones" style="text-transform:uppercase;width:100%;font-size:12px;">
     <thead>
         <tr>
             <th class="text-center">ID</th>
             <th class="text-center">#</th>
             <th class="text-center">DOC</th>
-            <th class="text-center">PEDIDO</th> 
+            <th class="text-center">PEDIDO</th>
             <th class="text-center">ALMACÉN</th>
             <th class="text-center">CLIENTE</th>
-            <th class="text-center">REGISTRADOR</th> 
+            <th class="text-center">REGISTRADOR</th>
             <th class="text-center">FECHA</th>
             <th class="text-center">TOTAL</th>
             <th class="text-center">ESTADO</th>
